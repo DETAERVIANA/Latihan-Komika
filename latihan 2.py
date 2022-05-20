58447dc0101edb373c9f45ff246cec5f598152e8
Deta Erviana <86216993+DETAERVIANA@users.noreply.github.com>
2022-05-20 20:31:12 +0700

    Latihan Komika ke 2

---
+++ latihan 2.py
@@ -0,0 +1,2 @@
+Nama = input("\nMasukkan Nama :")
+print("Halo", Nama,","" Selamat Datang di Kelas Pemula Python")
---
+++ latihan 9.py
@@ -0,0 +1,11 @@
+Tekanan = float(input("Masukkan Tekanan :"))
+print("Tekanan :",Tekanan,"Kilopascal")
+
+Tekanan_psi = Tekanan*0.159004
+print("Tekanan :", Tekanan_psi,"psi")
+
+Tekanan_mmHg = Tekanan_psi*65.5789
+print("Tekanan :",Tekanan_mmHg,"mmHg")
+
+Tekanan_atm = Tekanan*0.57090830
+print("Tekanan :",Tekanan_atm,"atm")
